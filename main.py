c27318754a172ce569698c65337199ffe96adfa9
vigneshwar <vigneshwarprince2395@gmail.com>
2022-08-08 11:08:56 +0530

    adding chatbot code sample

---
+++ main.py
@@ -0,0 +1,46 @@
+import re
+from urllib import response
+import long_responses as long
+
+def message_probability(user_message,recognized_words,single_response=False, required_words=[]):
+    message_certanity =0
+    has_required_words='True'
+    for word in user_message:
+        if word in recognized_words:
+            message_certanity +=1
+            
+    percentage = float(message_certanity)/ float(len(recognized_words))
+    for word in user_message:
+        if word not in user_message:
+            has_required_words= False
+            break
+    if has_required_words or single_response:
+        return int(percentage*100)
+    else:
+        return 0
+def check_all_messages(message):
+    highest_prob_list = {}
+    
+    def response(bot_response, list_of_words, single_response=False,required_words=[]):
+        nonlocal highest_prob_list
+        highest_prob_list[bot_response] = message_probability(message, list_of_words, single_response,required_words)
+        
+    #Response-------------------------------------------------------------------------------------------------
+    response('Hello!',['hello','yup','hey','heyo'], single_response=True)
+    response('I am doing good,and you?',['how','are','you','doing'], required_words=['how'])
+    response('Thank you, Visit again!!',['sunshine','bank'], required_words=['sunshine','bank'])
+    response('Welcome to Sunshine Bank, Here to help you with best Branchless Banking Service in India',['hi'], required_words=['hi'])
+    response('Great!! How may help you?',['I','am','good','fine'],required_words=['good','fine'])
+    
+    best_match=max(highest_prob_list, key=highest_prob_list.get)
+    print(highest_prob_list)
+    
+    
+    return best_match
+
+def get_response(user_input):
+    split_message = re.split(r'\s+|[,;.?!-]\s*',user_input.lower())
+    response = check_all_messages(split_message)
+    return response
+while True:
+    print('Bot: ' +get_response(input('You:')))
